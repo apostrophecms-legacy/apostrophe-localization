f515a66d5a4ae9927dc870efb144f8134bf2e711
Tom Boutell <tom@punkave.com>
2015-07-14 10:35:20 -0400

    up and running

---
+++ README.md
@@ -0,0 +1,120 @@
+# apostrophe-localization
+
+Allows internationalization, translation and localization of content on an [Apostrophe site](http://apostrophenow.org).
+
+## Installation
+
+First make sure you have an [Apostrophe project](http://apostrophenow.org)!
+
+Then:
+
+```
+npm install --save apostrophe-i18n
+```
+
+## Configuration
+
+In `app.js`, add the module to your configuration. Specify the default locale (generally the native language of the primary content creators) and an array of additional locales to be supported:
+
+```
+... other modules ...
+'apostrophe-localization': {
+  defaultLocale: 'en',
+  locales: { 'en': 'English', 'es': 'Español'};
+}
+```
+
+The keys in the `locales` object should be standard international language and/or culture codes. The values should be labels, written in the language in question, suitable for display by a locale picker. The default locale should be included, typically as the first choice.
+
+Optionally, add the locale picker to `outerLayout.html` wherever you see fit:
+
+{{ aposLocalePicker() }}
+
+The locale picker markup is deliberately easy to style, but you may also ignore it in favor of writing your own, as long as you generate the same links.
+
+That's it!
+
+## Internationalizing content
+
+To translate content, just log in as you normally would and use the locale picker. At first you will continue to see content for the default locale. However, any edits to content areas and singletons that you make while you are switched to an alternate locale will be saved as a separate version for that locale only.
+
+## Cross-cultural areas and singletons
+
+If you wish, you can specify that certain area and singleton names should not be translatable. Any edits made to these are shared by all locales:
+
+```
+... other modules ...
+'apostrophe-i18n': {
+  ...
+  universal: [ 'banner', 'thumbnail' ]
+}
+```
+
+If you need to, you can lock these down to a specific page or snippet type:
+
+```
+... other modules ...
+'apostrophe-i18n': {
+  ...
+  universal: [ 'home:banner', 'event:thumbnail' ]
+}
+```
+
+## Cross-cultural properties beyond areas and singletons
+
+By default, only areas, singletons, and the `title` property of each document are internationalized.
+
+Sometimes it is appropriate to internationalize other schema fields too, particularly fields of type `string`, but it's not safe for us to assume that.
+
+So, spell out the additional document properties you want to internationalize:
+
+```
+... other modules ...
+'apostrophe-i18n': {
+  ...
+  localized: [ 'summary', 'teaser' ]
+}
+```
+
+Again, you can lock these down to specific document type names:
+
+```
+... other modules ...
+'apostrophe-i18n': {
+  ...
+  localized: [ 'event:summary', 'blogPost:teaser' ]
+}
+```
+
+## Under the hood: what it really looks like in the database
+
+All localized properties are replicated inside a `localized` object (please pardon my bad Spanish):
+
+```
+{
+  title: 'About Us',
+  localized: {
+    en: {
+      title: 'About Us',
+    },
+    es: {
+      title: 'Sobre Nosotros'
+    }
+  }
+}
+
+However, Apostrophe automatically swaps the locale you really wanted to and from `title` and `body` when you fetch or store content. So **code that relies on `title` and `body` still works normally**.
+
+**In the database, `title` and `body` are a copy of the default locale's content.** This provides reasonable fallback behavior for code that does not use `apos.get` and `apos.putPage` in a typical manner. It also allows a site to work immediately when localization is added later.
+
+This simple approach to storing internationalized content allows the module to be implemented as straightforward enhancements to `apos.get` and `apos.putPage`.
+
+## Accessing the default locale's content while viewing another locale
+
+If you need access to the content for the default locale, you can access it like so:
+
+```
+page.localized.en.title
+```
+
+**However, for performance reasons, widget loaders are NOT called for content in other locales.** Otherwise the performance of the site would drop quickly as locales are added.
---
+++ index.js
@@ -0,0 +1,232 @@
+module.exports = factory;
+var deep = require('deep-get-set');
+var _ = require('lodash');
+var async = require('async');
+
+function factory(options, callback) {
+ return new Construct(options, callback);
+}
+
+function Construct(options, callback) {
+  var self = this;
+  self.options = options;
+  self._apos = options.apos;
+  self._app = options.app;
+
+  self.universal = options.universal || [];
+  self.localized = [ 'title' ].concat(options.localized || []);
+  self.locales = options.locales;
+  self.defaultLocale = options.defaultLocale;
+  self._apos.mixinModuleAssets(self, 'i18n', __dirname, options);
+
+  self._action = '/apos-i18n';
+
+  self._apos.addLocal('aposLocalePicker', function() {
+    var currentLocale = self._apos._aposLocals.getLocale();
+    var currentUrl = self._apos._aposLocals.getUrl();
+    var locales = [];
+
+    _.each(self.locales, function(label, locale) {
+      newUrl = '/' + locale + currentUrl;
+
+      var localeObject = {
+        label: label,
+        url: newUrl,
+        active: (currentLocale === locale)
+      };
+      locales.push(localeObject);
+    });
+    return self.render('localePicker', { locales: locales });
+  });
+
+  self.setLocale = function(req, locale) {
+    req.session.locale = locale;
+    req.locale = locale;
+  };
+
+  self.middleware = [
+
+    // if a locale is set in the session, set req.locale based on that
+
+    function(req, res, next) {
+      if (req.session.locale) {
+        req.locale = req.session.locale;
+      } else {
+        req.locale = self.defaultLocale;
+      }
+      return next();
+    },
+
+    // recognize /en/about; set req.locale accordingly, then remove the prefix
+    // so that routing can continue normally
+
+    function(req, res, next) {
+      if (req.method !== 'GET') {
+        return next();
+      }
+      var matches = req.url.match(/^\/(\w+)(\/.*|)$/);
+      if (!matches) {
+        return next();
+      }
+      if (!_.has(self.locales, matches[1])) {
+        return next();
+      }
+      req.locale = matches[1];
+      req.session.locale = req.locale;
+      req.url = matches[2];
+      if (!req.url.length) {
+        req.url = '/';
+      }
+      return next();
+    }
+
+  ];
+
+  var superBeforePutPage = self._apos.beforePutPage;
+
+  self._apos.beforePutPage = function(req, page, callback) {
+
+    ensureProperties(page, req);
+
+    // We translate top-level properties specifically called out for translation,
+    // plus all top-level areas not specifically denied translation. A recursive
+    // walk of all areas is a bad idea here because it would make more sense
+    // to just translate the entire top-level area. -Tom
+
+    _.each(page, function(value, key) {
+      if (!isArea(value)) {
+        return;
+      }
+      if (isUniversal(page, key)) {
+        return;
+      }
+      page.localized[req.locale][key] = value;
+      // Revert .body to the default culture's body, unless
+      // that doesn't exist yet, in which case we let it spawn from
+      // the current culture
+      if (_.has(page.localized[self.defaultLocale], key)) {
+        page[key] = page.localized[self.defaultLocale][key];
+      } else {
+        page.localized[self.defaultLocale][key] = page[key];
+      }
+    });
+
+    // Other properties are localized only if they are on the list.
+
+    _.each(self.localized, function(name) {
+      name = localizeForPage(page, name);
+      if (!name) {
+        return;
+      }
+      page.localized[req.locale][name] = page[name];
+      // Revert .title to the default culture's title, unless
+      // that doesn't exist yet, in which case we let it spawn from
+      // the current culture
+      if (_.has(page.localized[self.defaultLocale], name)) {
+        page[name] = page.localized[self.defaultLocale][name];
+      } else {
+        page.localized[self.defaultLocale][name] = page[name];
+      }
+    });
+
+    return superBeforePutPage(req, page, callback);
+  };
+
+  var superAfterGet = self._apos.afterGet || function(req, results, callback) {
+    return callback(null);
+  };
+
+  self._apos.afterGet = function(req, results, callback) {
+    var pages = results.pages;
+
+    if (!pages) {
+      // This is a query we don't deal with, such as a distinct query
+      return superAfterGet(req, pages, callback);
+    }
+
+    _.each(pages, function(page) {
+      ensureProperties(page, req);
+
+      // We translate top-level properties specifically called out for translation,
+      // plus all top-level areas not specifically denied translation. A recursive
+      // walk of all areas is a bad idea here because it would make more sense
+      // to just translate the entire top-level area. -Tom
+
+      _.each(page, function(value, key) {
+        if (!isArea(value)) {
+          return;
+        }
+        if (isUniversal(page, key)) {
+          return;
+        }
+        if (!_.has(page.localized[req.locale], key)) {
+          return;
+        }
+        page[key] = page.localized[req.locale][key];
+      });
+
+      // Other properties are localized only if they are on the list.
+
+      _.each(self.localized, function(name) {
+        name = localizeForPage(page, name);
+        if (!name) {
+          return;
+        }
+        if (!_.has(page.localized[req.locale], name)) {
+          return;
+        }
+        page[name] = page.localized[req.locale][name];
+      });
+
+    });
+
+    return superAfterGet(req, pages, callback);
+  };
+
+  // Invoke the callback. This must happen on next tick or later!
+  return process.nextTick(function() {
+    return callback(null);
+  });
+
+  function ensureProperties(page, req) {
+    if (!_.has(page, 'localized')) {
+      page.localized = {};
+    }
+    if (!_.has(page.localized, req.locale)) {
+      page.localized[req.locale] = {};
+    }
+    if (!_.has(page.localized, req.defaultLocale)) {
+      page.localized[req.defaultLocale] = {};
+    }
+  }
+
+  function isArea(value) {
+    if ((!value) || (value.type !== 'area')) {
+      return false;
+    }
+    return true;
+  }
+
+  function isUniversal(page, key) {
+    if (_.contains(self.options.universal || [], key) || _.contains(self.options.universal || [], page.type + ':' + key)) {
+      return;
+    }
+  }
+
+  function localizeForPage(page, name) {
+    var matches = name.match(/[\w+]:[\w+]/);
+    if (matches) {
+      if (page.type !== matches[1]) {
+        return;
+      }
+      name = matches[2];
+    }
+    if (!_.has(page, name)) {
+      return;
+    }
+    return name;
+  }
+};
+
+// Export the constructor so others can subclass
+factory.Construct = Construct;
